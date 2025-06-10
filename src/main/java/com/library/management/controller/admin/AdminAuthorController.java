fab7b2d619fde4e52db8c5e38b5ece968f29a2c9
nurbaanuarov <220103031@stu.sdu.edu.kz>
2025-06-11 01:21:32 +0500

    completed part of authors

--- src/main/java/com/library/management/controller/admin/AdminAuthorController.java
+++ src/main/java/com/library/management/controller/admin/AdminAuthorController.java
@@ -1,31 +1,35 @@
 package com.library.management.controller.admin;
 
 import com.library.management.model.Author;
+import com.library.management.service.AuthorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/admin/authors")
 @RequiredArgsConstructor
 public class AdminAuthorController {
 
-//    private final AuthorService authorService;
+    private final AuthorService authorService;
 
     @GetMapping
     public String listAuthors(Model model) {
-//        model.addAttribute("authors", authorService.findAll());
-//        model.addAttribute("author", new Author());
+        model.addAttribute("authors", authorService.findAll());
+        model.addAttribute("author", new Author());
         return "admin/authors";
     }
 
     @PostMapping("/new")
     public String createAuthor(@ModelAttribute Author author) {
-//        authorService.save(author);
+        authorService.save(author);
+        return "redirect:/admin/authors";
+    }
+
+    @PostMapping("/delete")
+    public String deleteAuthor(@RequestParam("id") Long id) {
+        authorService.delete(Author.builder().id(id).build());
         return "redirect:/admin/authors";
     }
 }
