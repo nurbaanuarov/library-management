6d3b1e596efd0b091f3af28dd82c2bb2ea1ed7da
nurbaanuarov <220103031@stu.sdu.edu.kz>
2025-06-12 17:53:32 +0500

    added author name on loading

--- src/main/java/com/library/management/service/impl/BookServiceImpl.java
+++ src/main/java/com/library/management/service/impl/BookServiceImpl.java
@@ -1,5 +1,6 @@
 package com.library.management.service.impl;
 
+import com.library.management.dao.AuthorDAO;
 import com.library.management.dao.BookCopyDAO;
 import com.library.management.dao.BookDAO;
 import com.library.management.exception.BookNotFoundException;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.function.Function;
 
 @Service
 @Transactional
@@ -18,11 +20,14 @@ import java.util.List;
 public class BookServiceImpl implements BookService {
     private final BookCopyDAO copyDao;
     private final BookDAO bookDAO;
+    private final AuthorDAO authorDAO;
 
     @Override
     @Transactional(readOnly = true)
     public List<Book> findAll() {
-        return bookDAO.findAll();
+        return bookDAO.findAll().stream()
+                .map(addAuthorName())
+                .toList();
     }
 
     @Override
@@ -47,5 +52,12 @@ public class BookServiceImpl implements BookService {
     @Override
     public void deleteById(Long id) {
         bookDAO.deleteById(id);
+    }
+
+    private Function<Book,Book> addAuthorName() {
+        return book -> {
+            authorDAO.findById(book.getAuthor().getId()).ifPresent(book::setAuthor);
+            return book;
+        };
     }
 }
